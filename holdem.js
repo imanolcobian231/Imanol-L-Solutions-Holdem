b9044f848f9dafcdb98cdb8a30b4439d28327abf
Imanol <olysseagroup@gmail.com>
2025-06-27 00:51:11 -0600

    detecta manos (con libreria)

--- holdem.js
+++ holdem.js
@@ -4,12 +4,14 @@ let comunityCards = []
 let holeCards = []
 let used = new Set()
 let card
+const { Hand } = require("pokersolver");
 
 //Genera carta sumando los valores aletorios de los numeros y palos
 function generateCards() {
     do {
         card = numbers[Math.floor(Math.random()*numbers.length)] + suits[Math.floor(Math.random()*suits.length)]
     } while(used.has(card))
+
     used.add(card) //Agrega las cartas a un set para que no se repitan
     return card
 }
@@ -32,7 +34,6 @@ function generateHoleCards() {
 
 //Agrega valor a los numeros y acomoda en base al valor
 function ranks() {
-    let compare = [...holeCards, ...comunityCards]
     const valores = {"2": 2, "3": 3, "4": 4, "5": 5,"6": 6, "7": 7, "8": 8, "9": 9,"10": 10, "J": 11, "Q": 12, "K": 13, "A": 14};
     comunityCards.sort((a, b) => {
         let valA = valores[a.slice(0, -1)];//Elimina el palo para poder comparar el numero
@@ -40,11 +41,20 @@ function ranks() {
         return valB - valA;//Ordena de mayor a menor 
     });
 
-    return comunityCards;
+    console.log("ranks: ",comunityCards)
+    return ""
+}
+
+function hands() {
+    let compare = [...holeCards, ...comunityCards]
+    let hand = Hand.solve(compare)
+    console.log("type: ", hand.name)
+    return ""
 }
 
 
 console.log(generateComunityCards())
 console.log(generateHoleCards())
 console.log(ranks())
+console.log(hands())
 
