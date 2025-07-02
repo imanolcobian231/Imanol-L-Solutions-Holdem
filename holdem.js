b00fa88ee5b380f2fff0cbf91836869659233f35
Imanol <olysseagroup@gmail.com>
2025-07-02 10:59:44 -0600

    avance detector de manos

--- holdem.js
+++ holdem.js
@@ -46,14 +46,28 @@ function ranks() {
     return ""
 }
 
+//Detecta manos
+function hands() {
+    let compare = [...holeCards, ...comunityCards]
+    let hand = Hand.solve(compare)
+    console.log("type: ", hand.name) //Obtiene tipo de mano 
+    return ""
+}
+
 function handsR() {
     let compare1 = []
     let compare = []
+    let pairs = false
+    let three = false
+    let twoPairs = 0
     compare = [...holeCards, ...comunityCards]
 
+    //Elimina palo para comparar numero
     for (let i = 0; i < compare.length; i++) {
         compare1.push(compare[i].slice(0, -1))
     }
+
+    //Cuenta cantidad de repeticiones
     const conteo = {} 
     for (const num of compare1) {
         if(conteo[num]) {
@@ -62,9 +76,82 @@ function handsR() {
             conteo[num] = 1
         }
     }
-        return conteo
+
+    //Valida si hay pares para no repetir"HIGH CARD"
+    for(const num in conteo) {
+        if (conteo[num] > 1) {
+            pairs = true
+            break
+        }
+    }
+
+    //Valida par, trio, pokar, dos pares y full house
+    for(const num in conteo) {
+        if(pairs === false) {
+            console.log("type: high card")
+            break
+        }
+
+        if(conteo[num] == 2) {
+            twoPairs++
+        }
+
+        if(conteo[num] == 3) {
+            three = true
+        }
+
+        if(conteo[num] == 4) {
+            console.log("type: four of a kind")
+        }
+    }
+
+    if(twoPairs == 1) { //TWO PAIRS
+        console.log("type: pair")
+    }
+
+    if(three && twoPairs >= 1) { //FULLHOUSE
+        console.log("type: full house")
+    }
+
+
+
+    if(twoPairs >= 2) { //FULLHOUSE
+        console.log("type: two pair")
+    }
+    return conteo
     } 
 
+
+    //Valida flush
+function flush() {
+    let compare = []
+    let compare1 = []
+    const conteo = {}
+    compare = [...holeCards, ...comunityCards]
+    //Elimina numero para comparar palo
+    for (let i = 0; i < compare.length; i++) {
+        compare1.push(compare[i].slice(1))
+    }
+
+    //Cuenta repeticiones de palo
+    for (const num of compare1) {
+        if(conteo[num]) {
+            conteo[num]++
+        } else {
+            conteo[num] = 1
+        }
+    }
+
+    //Si se repite 5 veces retorna "FLUSH"
+    for(const num in conteo) {
+        if(conteo[num] >= 5) {
+            console.log("type: flush")
+        }
+    }
+
+    return conteo
+    
+}
 
 
 console.log(generateComunityCards())
@@ -72,4 +159,5 @@ console.log(generateHoleCards())
 console.log(ranks())
 console.log(hands())
 console.log(handsR())
+console.log(flush())
 
